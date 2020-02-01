097b5365672bafd98df90e893b38675fe4b0b9f6
David Mitjana Castro <d.mitjana@gmail.com>
2020-02-01 18:12:20 +0100

    initial commit

---
+++ src/components/Videocall.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Videocall() {
+  return (
+    <div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </div>
+  );
+}
+
+export default Videocall;
