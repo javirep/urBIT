534d95f0b192fed1ce423b97a322de79d3dd6587
Javier Repetto de los Dolores <javierrepetto@MacBook-Pro-de-Javier.local>
2020-02-01 18:17:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <iframe
+          src="https://tokbox.com/embed/embed/ot-embed.js?embedId=145a87b7-5924-4d5f-93f7-5bbba24b0ba6&room=DEFAULT_ROOM&iframe=true"
+          width="800"
+          height="640"
+          scrolling="auto"
+          allow="microphone; camera"
+        ></iframe>
+      
     </div>
   );
 }
